3b2669e65462e5337b279dc22e2a10c55e4a2010
xiangfuli <xiangfuli96@gmail.com>
2023-10-08 19:34:31 -0400

    Successfully get data from simulator

--- src/apis/api_client.cpp
+++ src/apis/api_client.cpp
@@ -16,8 +16,14 @@ int ApiClient::set_actuators(float a1, float a2, float a3, float a4) {
   commands.add_actuator_commands(a4);
   grpc::Status status = this->stub_->set_actuators(&context, commands, &response);
   if (status.ok()) {
+    std::cout << "RPC returns: OK" << std::endl;
+    std::cout << "RPC response: " << response.actuator_commands(0) << std::endl;
+    std::cout << "RPC response: " << response.actuator_commands(1) << std::endl;
+    std::cout << "RPC response: " << response.actuator_commands(2) << std::endl;
+    std::cout << "RPC response: " << response.actuator_commands(3) << std::endl;
     return 0;
   } else {
+    std::cerr << status.error_code() << std::endl;
     std::cerr << "RPC failed: " << status.error_message() << std::endl;
     return -1;
   }
--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "apis/api_client.hpp"
 
 int main() {
-  std::string server_address = "0.0.0.0:30031";
+  std::string server_address = "104.39.190.31:30031";
 
   ApiClient* apiClient = new ApiClient(grpc::CreateChannel(server_address, grpc::InsecureChannelCredentials()));
 
